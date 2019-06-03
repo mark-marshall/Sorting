ddd491950b7467d9e6902a32b41a24ac7e4127fa
mark-marshall <mark.marshallgp@gmail.com>
2019-06-03 14:09:07 +0100

    Implemented binary_search - iterative

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -8,15 +8,22 @@ def linear_search(arr, target):
 
 # STRETCH: write an iterative implementation of Binary Search 
 def binary_search(arr, target):
-
   if len(arr) == 0:
-    return -1 # array empty
-    
+    return -1 # array empty 
   low = 0
   high = len(arr)-1
-
-  # TO-DO: add missing code
-
+  # loop while low remains smaller than high
+  while low <= high:
+    mp = (low + high) // 2
+    # if the el matches return the index
+    if arr[mp] == target:
+      return mp
+    # if the mp is too low, adjust high
+    elif arr[mp] > target:
+      high = mp
+    # if the mp is too high, adjust low
+    else:
+      low = mp
   return -1 # not found
 
 
